affe8a1cb642d2e434cd91642735b465769ceff2
Pranav Deshmukh <pranavdeshmukh5454@gmail.com>
2025-12-17 13:07:55 +0530

    Setup Prisma 6 for deployment

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,17 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor(private config: ConfigService) {
-    const connectionString = config.get<string>('DATABASE_URL');
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-    
-    super({ adapter });
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get<string>('DATABASE_URL'),
+        },
+      },
+    });
   }
 
   async onModuleInit() {
